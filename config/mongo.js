a47718c39d6ea53af6a3abd39dfa5ae6334d313c
vishnu k <vishnukkakkarayil@gmail.com>
2023-03-11 12:12:51 +0530

    code formatted

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+const url = process.env.MONGOOSE_URL
 
-mongoose.connect("mongodb://127.0.0.1:27017/gamezone",
+mongoose.connect(url,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
